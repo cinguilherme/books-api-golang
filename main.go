6b0158d6f9cf83afb32a3e8eecd1034eb70e135b
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-06-25 16:55:32 -0300

    minimal endpoints in place

--- main.go
+++ main.go
@@ -3,13 +3,41 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	
 )
+
+type Dependency struct {
+	name string `json:"name"`
+	status string `json:"status"`
+}
+
+type Health struct {
+	dependencies []Dependency `json:"dependencies"`
+}
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})    
+	})
+
+	r.GET("/health", func(c *gin.Context) {
+		
+		dependencies := &Health{
+			dependencies: []Dependency{ 
+				Dependency{ name: "one", status: "pending"}}}
+
+		
+		c.JSON(http.StatusOK, 
+			gin.H{"data": "hello world", 
+			"dependencies": dependencies})  
+	})
+
+	r.GET("/books", func(c *gin.Context) {
+				
+		c.JSON(http.StatusOK, 
+			gin.H{"data": "books"})  
 	})
 
 	r.Run()
